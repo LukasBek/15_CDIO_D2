51bd81145a635a97dba32d9355c94704be2f5c2c
SebbyG <seb.hop@hotmail.com>
2016-03-31 13:29:08 +0200

    Tilføjet testklasser

---
+++ src/test/TestClient.java
@@ -0,0 +1,22 @@
+package test;
+
+import client.Client;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestClient{
+	
+	@Before
+	public void initClient(){
+		Client c = new Client();
+	}
+	
+	@Test
+	public void shutDown(){
+		
+	}
+}
+
+
